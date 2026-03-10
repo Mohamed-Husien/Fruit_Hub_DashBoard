a40472d094dca81467a312a71b86693d9a831aa3
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2026-03-10 05:56:22 +0200

    add supabase

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,16 @@ import 'package:fruit_hub_dashboard/core/services/custom_bloc_observer.dart';
 import 'package:fruit_hub_dashboard/core/services/get_it_service.dart';
 import 'package:fruit_hub_dashboard/features/dashboard/presentation/views/dash_board_view.dart';
 import 'package:fruit_hub_dashboard/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   setupGetit();
   WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://mzbywmzboxahxuqbvsja.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16Ynl3bXpib3hhaHh1cWJ2c2phIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMjQ4OTgsImV4cCI6MjA4ODYwMDg5OH0.pJSNtp2kVoTb2Lp6NqQ4jf_kP22zwcHyBKZfgYWY6Z8',
+  );
   Bloc.observer = CustomBlocObserver();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
